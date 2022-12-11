1a9e022dc7602b6a337da63a9a8a7c1848b30d4c
Ishwor Shrestha <ishwor.shrestha@amniltech.com>
2022-12-11 08:42:17 +0545

    Created database, migrated & updated using EF tools

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList")); // use connection string accrding to database - "TodoList" | LinQ helps to switch DB
+    opt.UseSqlite("Data Source= todo.db")); // use connection string accrding to database - "TodoList" | LinQ helps to switch DB
     
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

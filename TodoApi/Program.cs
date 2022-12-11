5f45ee126fb960b23efcc23eaacf527cc9ec25ce
Ishwor Shrestha <ishwor.shrestha@amniltech.com>
2022-12-11 07:32:46 +0545

    Todo Model added ...

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+    opt.UseInMemoryDatabase("TodoList")); // use connection string accrding to database - "TodoList"
     
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
